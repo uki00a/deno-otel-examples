32a6c60e3baf48430df2a2362dd0efea3b808c79
Yuki Tanaka <uki00a@gmail.com>
2025-02-22 14:34:04 +0900

    feat: add `http` example

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # deno-otel-examples
+
+## Requirements
+
+- Deno
+- [otelcol](https://github.com/open-telemetry/opentelemetry-collector)
+
+## How to run a example
+
+1. Start OpenTelemetry Collector
+
+```shell
+$ deno task otelcol:start
+```
+
+2. Run an example
+
+```shell
+$ deno task example:http
+```
---
+++ examples/http.js
@@ -0,0 +1,10 @@
+const server = Deno.serve({
+  hostname: "localhost",
+  port: 0,
+  onListen: ({ hostname, port }) => {
+    fetch(`http://${hostname}:${port}`)
+      .then((res) => res.text())
+      .then((body) => console.info(body))
+      .finally(() => server.shutdown());
+  },
+}, (_req) => new Response("OK"));
